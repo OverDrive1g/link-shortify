e9e0bbca79770e8f090c71ad5d01078caf90039a
Bedilo Vladislav <bedilova@answerpro.org>
2023-05-16 18:31:56 +0300

    📦 main feature

--- main.ts
+++ main.ts
@@ -1,12 +1,29 @@
-const fastify = require('fastify')({ logger: true })
+import Fastify, { FastifyReply, FastifyRequest } from 'fastify'
+import { nanoid } from 'nanoid'
 
-// Declare a route
-fastify.get('/', async (request:any, reply:any) => {
-  return { hello: 'world' }
+const fastify = Fastify({ logger: true })
+
+const url_map:{[key:string]:string} = {}
+
+fastify.get('/:id', async (request:FastifyRequest, reply:FastifyReply) => {
+  const gotoId = (request.params as any).id
+  if(!url_map[gotoId]){
+    return {
+      error:`link with id=${gotoId} not found`
+    }
+  }
+  reply.redirect(301, url_map[gotoId])
 })
 
-// Run the server!
-const start = async () => {
+fastify.post('/', async (request:any, reply:any) => {
+    const newId = nanoid(5)
+    
+    url_map[newId] = request.body
+
+    return `/${newId}`
+})
+
+  const start = async () => {
   try {
     await fastify.listen({ port: 3000 })
   } catch (err) {
